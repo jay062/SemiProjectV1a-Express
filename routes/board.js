ed7ab2ba0d5036b630f50f25c2613018e5108707
jay062 <jay062@naver.com>
2023-02-16 11:31:44 +0900

    static 경로 수정, img, css 파일 추가

--- routes/board.js
+++ routes/board.js
@@ -2,6 +2,15 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+router.get('/list',(req, res) => {
+    res.sendFile(path.join(__dirname,'../public','list.html'));
+});
+router.get('/write',(req, res) => {
+    res.sendFile(path.join(__dirname,'../public','write.html'));
+});
+router.get('/view',(req, res) => {
+    res.sendFile(path.join(__dirname,'../public','view.html'));
+});
 
 
 module.exports = router;
